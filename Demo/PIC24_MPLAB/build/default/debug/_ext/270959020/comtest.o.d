a5c854fe57e01adc37af81d6597649a4b19e4c62
Acosta Guillermo Daniel <ga@dinalight.com>
2018-05-21 14:29:52 -0300

    Rtos Example modified

---
+++ Demo/PIC24_MPLAB/build/default/debug/_ext/270959020/comtest.o.d
@@ -0,0 +1,14 @@
+build/default/debug/_ext/270959020/comtest.o:  \
+../Common/Minimal/comtest.c  \
+ ../../Source/include/FreeRTOS.h  \
+FreeRTOSConfig.h  \
+ ../../Source/include/projdefs.h  \
+../../Source/include/portable.h  \
+ ../../Source/include/deprecated_definitions.h  \
+ ../../Source/portable/MPLAB/PIC24_dsPIC/portmacro.h  \
+ ../../Source/include/mpu_wrappers.h  \
+../../Source/include/task.h  \
+ ../../Source/include/list.h  \
+../Common/include/serial.h  \
+ ../Common/include/comtest.h  \
+../Common/include/partest.h 
---
+++ Demo/PIC24_MPLAB/build/default/debug/_ext/410575107/portasm_PIC24.o.d
@@ -0,0 +1,5 @@
+build/default/debug/_ext/410575107/portasm_PIC24.o:  \
+ ../../Source/portable/MPLAB/PIC24_dsPIC/portasm_PIC24.S 
+build/default/debug/_ext/410575107/portasm_PIC24.o:  \
+  \
+../../Source/portable/MPLAB/PIC24_dsPIC/portasm_PIC24.S  \
